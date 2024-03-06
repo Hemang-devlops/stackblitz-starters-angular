09393bdb5ca79571bd30a3cdb7ad8e82ebb905de
Hemang-devlops <136294289+Hemang-devlops@users.noreply.github.com>
2024-03-06 10:48:43 +0000

    header css changes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +7,28 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  constructor(public router: Router){}
+  menuType: String = 'default';
 
+  ngOnInit(): void {
+    this.router.events.subscribe((value : any) => {
+      if (value.url){
+        if(localStorage.getItem("seller") && value?.url?.includes('seller')) {
+          this.menuType = 'seller';
+          console.log('in seller',this.menuType);
+        }else{
+          this.menuType = 'default';
+          console.log('out seller',this.menuType);
+        }
+      }
+    })
+  }
+
+  logoutSeller(): void{
+    if (localStorage.getItem("seller")){
+      localStorage.setItem("seller", "");
+      this.router.navigate(["/"]);
+    }
+  }
 }
